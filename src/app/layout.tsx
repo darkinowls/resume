fd9bf3c8fd04285be1772bca5cc2c45b74c59bbd
Denys Chernousov <blackInOwls@gmail.com>
2024-03-14 17:25:04 +0200

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import resumeConfig from '../../edit-me/config/resumeConfig';
 
 // ICONS CONFIG
 import { config } from '@fortawesome/fontawesome-svg-core';
+
 config.autoAddCss = false;
 
 // STYLES
@@ -84,10 +85,6 @@ const RootLayout: React.FC<PropsWithChildren> = async ({ children }) => {
         resumeConfig.appTheme === ThemeSetting.Dark && 'dark',
       )}
     >
-      <Head>
-        <link rel="shortcut icon" href="/edit-me/favicon.png" />
-      </Head>
-
       <body className="bg-neutral-1 text-neutral-12 selection:bg-accent-11 selection:text-neutral-1">
         {children}
       </body>
